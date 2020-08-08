17e0855c1285093f36ef3c8cb3762149a16b5e0f
jacobhiggins <jdh4je@virginia.edu>
2020-08-08 10:47:20 -0400

    Update mpc_interface.h

--- mpc/mpc_interface.h
+++ mpc/mpc_interface.h
@@ -15,12 +15,6 @@
  *
  * 4. the input is available in the struct to be fed to the plant
  *
- * Based on  the characteristics of  the plant, the  following macros,
- * may need to be redefined:
- *
- * MPC_STATE_NUM, with the number of system states
- * MPC_INPUT_NUM, with the number of input to the system
- *
  * The shared memory is created and  removed by the MPC controller. In
  * case, the  default shared  memory key 0xC1A0  is already  used (for
  * example, because  another MPC controller  is up and  running), then
@@ -36,17 +30,24 @@
 #define MPC_SHM_KEY 0xC1A0  /* hard-coded key of shared mem SysV object */
 #define MPC_SHM_FLAGS 0666  /* we go easy: everybody reads and writes */
 
-#define MPC_STATE_NUM 12
-#define MPC_INPUT_NUM 4
-
 #define MPC_SEM_NUM            2
 #define MPC_SEM_STATE_WRITTEN  0   /* +1: plant; -1 MPC controller */
 #define MPC_SEM_INPUT_WRITTEN  1   /* +1: MPC controller; -1 plant */
 
 struct shared_data {
 	sem_t  sems[MPC_SEM_NUM];    /* semaphores to regulate communication */
-	double state[MPC_STATE_NUM]; /* UAV -> MPC controller */
-	double input[MPC_INPUT_NUM]; /* MPC controller -> UAV */
+	size_t state_num;            /* number of states */
+	size_t input_num;            /* number of inputs */
+	/*
+	 * The shared memory then continues with two arrays of double
+	 * whose size is dynamic:
+	 *
+	 *   double state[state_num]
+	 *     used by the application to communicate the state to MPC
+	 *
+	 *   double input[MPC_INPUT_NUM]
+	 *     used by MPC to communicate the input to the applciation
+	 */
 };
 
 #endif /* _MPC_INTERFACE_H_ */
